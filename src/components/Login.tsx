d9cdba066adf12d25b5adf2a6864a98c5f760220
aaronjohnsaldanha <aaronautics@gmail.com>
2025-08-26 02:38:23 +0400

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -45,7 +45,7 @@ const Login: React.FC = () => {
             />
           </div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Auntrack Calendar
+            HR Event Calendar
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Sign in to your account
